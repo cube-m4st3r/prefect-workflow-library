0a5a20402698b21af2de847a5285522627188fd8
cube-m4st3r <benjaminfinck02@googlemail.com>
2025-06-03 16:21:59 +0200

    feat: added image manipulation

---
+++ utils/image_manipulation.py
@@ -0,0 +1,24 @@
+from io import BytesIO
+from PIL import Image
+from collections import Counter
+
+
+class image_manipulation():
+
+    @classmethod
+    async def _get_highest_frequency_color_hex(cls, image_data):
+        image_bytes = BytesIO(image_data)
+
+        image = Image.open(image_bytes)
+
+        image = image.convert('RGB')
+
+        pixels = list(image.getdata())
+
+        color_count = Counter(pixels)
+
+        highest_frequency_color = max(color_count, key=color_count.get)
+
+        hex_color = '#{:02x}{:02x}{:02x}'.format(*highest_frequency_color)
+
+        return hex_color
